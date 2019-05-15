75b49aa4fea888b744bc596c340f8eac5be9f00a
eroubenoff <eroubenoff@gmail.com>
2019-05-15 15:02:29 -0700

    began adding k-fold verification

--- 06_SVM.py
+++ 06_SVM.py
@@ -15,31 +15,47 @@ import pickle
 from sklearn import svm
 from scipy import sparse
 import scipy as sp
-import random
 
 
 def train_model(tf_sparse, users, hashtags, training_data):
-    # np.set_printoptions(threshold=np.inf)
 
-    training_data_user = [x for x in training_data.keys()]
-    training_data_out = [x for x in training_data.values()]
-    tf_fit = tf_sparse[training_data_user, :]
+    tf_sparse = count_to_proportion(tf_sparse)
 
     hashtags_lookup = {v: k for k, v in hashtags.items()}
+    # This is the inverse of the hashtags dict.
+
+    tf_fit = tf_sparse[[users[x] for x in training_data.keys()], :]
 
     clf = svm.SVC()
-    fit = clf.fit(tf_fit, training_data_out)
+    fit = clf.fit(tf_fit, [x for x in training_data.values()])
     print(fit)
-
+"""
     for u in random.sample(list(users), 100):
         rows, cols = tf_sparse[users[u], :].nonzero()
         print([hashtags_lookup[x] for x in cols])
         print(fit.predict(tf_sparse[users[u], :]))
+"""
+
+def validate(tf_sparse, users, hashtags, training_data, k):
+    training_data_length = len(training_data)
+    ordered_training_data = list(training_data.items())
+
+    count = 0
+    for subset in range(k):
+        tmp = {}
+        j = 0
+        while j < (training_data_length // k):
+            obj = ordered_training_data[count]
+            tmp[obj[0]] = obj[1]
+            count += 1
+            j += 1
+        print(tmp)
+        train_model(tf_sparse, users, hashtags, tmp)
 
 
 def count_to_proportion(tf_sparse):
     rowsums = sp.sum(tf_sparse, axis=1)
-    tf_sparse = sparse.csr_matrix(tf_sparse, dtype = "d")
+    tf_sparse = sparse.csr_matrix(tf_sparse, dtype="d")
     return(tf_sparse.multiply(1/rowsums))
 
 
@@ -63,8 +79,7 @@ if __name__ == "__main__":
 
     # Sparse matrix
     tf_sparse = sparse.load_npz('tf_sparse.npz')
-    tf_sparse = count_to_proportion(tf_sparse)
-    # print(tf_sparse)
+
     # User hashmap
     with open('users.pickle', 'rb') as f:
         users = pickle.load(f)
@@ -77,3 +92,4 @@ if __name__ == "__main__":
         training_data = pickle.load(f)
 
     model = train_model(tf_sparse, users, hashtags, training_data)
+    # validate(tf_sparse, users, hashtags, training_data, 5)
